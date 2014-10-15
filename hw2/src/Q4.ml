4c2ec9d3fb37244081c992d3f6224819ae9eada5
Jie-Gang Kuang <jk3735@columbia.edu>
2014-10-15 14:19:00 -0400

    Add firewall in Q4

--- hw2/src/Q4.ml
+++ hw2/src/Q4.ml
@@ -1,12 +1,23 @@
 open NetKAT.Std
 
-let hub : policy =
+let flood : policy =
     <:netkat<
-        if port = 1 then port := 2 + port := 3 + port := 4
-        else if port = 2 then port := 1 + port := 3 + port := 4
-        else if port = 3 then port := 1 + port := 2 + port := 4
-        else if port = 4 then port := 1 + port := 2 + port := 3
+        port := 1 + port := 2 + port := 3 + port := 4
+    >>
+
+let pol_https_gate : policy =
+    <:netkat<
+        if tcpDstPort = 443 && (ethDst = 1 || ethDst = 2 || ethDst = 3) then drop
+        else $flood
+    >>
+
+let learning_switch : policy =
+    <:netkat<
+        if port = 1 then $pol_https_gate
+        else if port = 2 then $pol_https_gate
+        else if port = 3 then $pol_https_gate 
+        else if port = 4 then $flood
         else drop
     >>
 
-let _ = run_static hub
+let _ = run_static learning_switch
