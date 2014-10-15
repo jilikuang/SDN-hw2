dbff271ae1a3c7cf9d03df5d9ded4da7fc4630e8
Jie-Gang Kuang <jk3735@columbia.edu>
2014-10-15 11:32:08 -0400

    Simple test Q3

--- hw2/src/Q3.ml
+++ hw2/src/Q3.ml
@@ -1,12 +1,19 @@
 open NetKAT.Std
 
-let hub : policy =
+let pol_port1 : policy =
     <:netkat<
-        if port = 1 then port := 2 + port := 3 + port := 4
+        if ((tcpSrcPort = 22) && ((ethDst = 3) || ethDst = 4))
+        then drop
+        else port := 2 + port := 3 + port := 4
+    >>
+
+let switch : policy =
+    <:netkat<
+        if port = 1 then $pol_port1
         else if port = 2 then port := 1 + port := 3 + port := 4
         else if port = 3 then port := 1 + port := 2 + port := 4
         else if port = 4 then port := 1 + port := 2 + port := 3
         else drop
     >>
 
-let _ = run_static hub
+let _ = run_static switch
