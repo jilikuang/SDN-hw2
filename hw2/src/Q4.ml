3e6758a567511946f136654023770a23fb01b85d
Jie-Gang Kuang <jk3735@columbia.edu>
2014-10-15 17:01:36 -0400

    Copy learning switch in tutorial

--- hw2/src/Q4.ml
+++ hw2/src/Q4.ml
@@ -1,23 +1,65 @@
+open Core.Std
+open Async.Std
+open Async_NetKAT
+open NetKAT_Types
 open NetKAT.Std
 
-let flood : policy =
-    <:netkat<
-        port := 1 + port := 2 + port := 3 + port := 4
-    >>
+let known_hosts : (switchId * dlAddr, portId) Hashtbl.t =
+  Hashtbl.Poly.create ()
 
-let pol_https_gate : policy =
-    <:netkat<
-        if tcpDstPort = 443 && (ethDst = 1 || ethDst = 2 || ethDst = 3) then drop
-        else $flood
-    >>
+let learn (sw : switchId) (pt : portId) (pk : packet) : bool =
+  match Hashtbl.find known_hosts (sw, pk.dlSrc) with
+    | Some pt' when pt = pt' ->
+       false
+    | _ ->
+       ignore (Hashtbl.add known_hosts (sw, pk.dlSrc) pt);
+       true
 
-let learning_switch : policy =
-    <:netkat<
-        if port = 1 then $pol_https_gate
-        else if port = 2 then $pol_https_gate
-        else if port = 3 then $pol_https_gate 
-        else if port = 4 then $flood
-        else drop
-    >>
+let packet_out (sw : switchId) (pk : packet) : action =
+  match Hashtbl.find known_hosts (sw, pk.dlDst) with
+    | Some pt -> Output (Physical pt)
+    | None -> Output All
 
-let _ = run_static learning_switch
+let default =
+  <:netkat<port := "learn">>
+
+let learn_pol () =
+  List.fold_right
+    (Hashtbl.to_alist known_hosts)
+    ~init:default
+    ~f:(fun ((sw,addr),pt) pol ->
+	<:netkat<
+          if switch = $sw && ethSrc = $addr then drop else $pol
+	>>)
+
+let route_pol () =
+  List.fold_right
+    (Hashtbl.to_alist known_hosts)
+    ~init:default
+    ~f:(fun ((sw,addr),pt) pol ->
+      <:netkat<
+        if switch = $sw && ethDst = $addr then port := $pt else $pol
+      >>)
+
+let policy () =
+  let l = learn_pol () in
+  let r = route_pol () in
+  <:netkat< $l + $r >>
+
+let handler t w () e = match e with
+  | PacketIn(_, switch_id, port_id, payload, _) ->
+    let packet = Packet.parse (SDN_Types.payload_bytes payload) in
+    let pol =
+      if learn switch_id port_id packet then
+	Some (policy ())
+      else
+	None in
+    let action = packet_out switch_id packet in
+    Pipe.write w (switch_id, (payload, Some(port_id), [action])) >>= fun _ ->
+    return pol
+  | _ -> return None
+
+let _ =
+  Async_NetKAT_Controller.start
+    (create ~pipes:(PipeSet.singleton "learn") (policy ()) handler) ();
+  never_returns (Scheduler.go ())
