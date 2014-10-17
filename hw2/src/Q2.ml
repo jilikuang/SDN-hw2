628133743c8cd555f5a5aa347141aa5c0c0a7b0d
Jie-Gang Kuang <jk3735@columbia.edu>
2014-10-17 17:03:12 -0400

    Q2 half

--- hw2/src/Q2.ml
+++ hw2/src/Q2.ml
@@ -1,12 +1,18 @@
 open NetKAT.Std
 
-let hub : policy =
+let count = ref 0
+
+let inc_c (c: int ref) = c.contents <- c.contents + 1
+
+let counting_hub : policy =
     <:netkat<
-        if port = 1 then port := 2 + port := 3 + port := 4
-        else if port = 2 then port := 1 + port := 3 + port := 4
-        else if port = 3 then port := 1 + port := 2 + port := 4
-        else if port = 4 then port := 1 + port := 2 + port := 3
+        if port = 1 then port := 2 + port := 3 + port := 4 + port := 5 + port := 6
+        else if port = 2 then port := 1 + port := 3 + port := 4 + port := 5 + port := 6
+        else if port = 3 then port := 1 + port := 2 + port := 4 + port := 5 + port := 6
+        else if port = 4 then port := 1 + port := 2 + port := 3 + port := 5 + port := 6
+        else if port = 5 then port := 1 + port := 2 + port := 3 + port := 4 + port := 6
+        else if port = 6 then port := 1 + port := 2 + port := 3 + port := 4 + port := 6
         else drop
     >>
 
-let _ = run_static hub
+let _ = run_static counting_hub
