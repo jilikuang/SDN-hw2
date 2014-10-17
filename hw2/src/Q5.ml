b2cc305a72bdd135155e951088e9d84d4e13cea4
Jie-Gang Kuang <jk3735@columbia.edu>
2014-10-17 17:02:41 -0400

    Q5 half

--- hw2/src/Q5.ml
+++ hw2/src/Q5.ml
@@ -1,12 +1,30 @@
+(* Refer to an example in
+   https://gitbug.com/frenetic-lang/frenetic/blob/master/example/tree-2-2.kat *)
 open NetKAT.Std
 
-let hub : policy =
+let pkg_sent = [| 0; 0; 0; 0 |]
+
+let pkg_rcvd = [| 0; 0; 0; 0 |]
+
+let inc_cnt arr idx = arr.(idx) <- arr.(idx) + 1
+
+let h_switch =
     <:netkat<
-        if port = 1 then port := 2 + port := 3 + port := 4
-        else if port = 2 then port := 1 + port := 3 + port := 4
-        else if port = 3 then port := 1 + port := 2 + port := 4
-        else if port = 4 then port := 1 + port := 2 + port := 3
+        if switch = 1 then
+            if port = 1 then port := 2
+            else if port = 2 then port := 1
+            else drop
+        else if switch = 2 then
+            if ethDst = 1 then port := 1
+            else if ethDst = 2 then port := 2
+            else if (ethDst = 3 || ethDst = 4) then port := 3
+            else drop
+        else if switch = 3 then
+            if ethDst = 3 then port := 1
+            else if ethDst = 4 then port := 2
+            else if (ethDst = 1 || ethDst = 2) then port := 3
+            else drop
         else drop
     >>
 
-let _ = run_static hub
+let _ = run_static h_switch
