13d4ab0d09ece46528e21cf1d087191d66c31865
Jie-Gang Kuang <jk3735@columbia.edu>
2014-10-18 21:20:56 -0400

    Correct Q3

--- hw2/src/Q3.ml
+++ hw2/src/Q3.ml
@@ -3,22 +3,18 @@ open NetKAT.Std
 let flood : policy =
     <:netkat<
         if port = 1 then port := 2 + port := 3 + port := 4
-        else if port = 2 then port := 1 + port := 2 + port := 4
+        else if port = 2 then port := 1 + port := 3 + port := 4
         else if port = 3 then port := 1 + port := 2 + port := 4
         else if port = 4 then port := 1 + port := 2 + port := 3
         else drop
     >>
 
-let pol_h1 : policy =
-    <:netkat<
-        if (tcpSrcPort = 22 && (ethDst = 3 || ethDst = 4))
-        then drop
-        else $flood
-    >>
-
 let switch : policy =
     <:netkat<
-        if ethSrc = 1 then $pol_h1
+        if ethSrc = 1
+           && ethType = 0x0800 && ipProto = 0x06 && tcpDstPort = 22 &&
+           (ethDst = 3 || ethDst = 4)
+        then drop
         else $flood
     >>
 
